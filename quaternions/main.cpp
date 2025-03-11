c795817ae802f903bee20ba6ec5ca46e32b65646
ericb <ericbmousy@gmail.com>
2025-03-11 20:53:07 +0100

    test geodesic distance function

--- quaternions/main.cpp
+++ quaternions/main.cpp
@@ -1,30 +1,10 @@
+#include "vectors.h"
 #include "quaternions.h"
 #include <iostream>
 
 int main() {
-	// Initialization test
-	Quaternion q1;
-	Quaternion q2(0, 2, 3, 4);
-	Quaternion q3(2, { -4, -3, -1 });
-
-	// Boolean test
-	std::cout << q1.isZero() << " " << q1.isIdentity() << "\n";
-	// operator<< test
-	std::cout << q2 << "\n";
-	// Addition test
-	std::cout << q2 + q3 << "\n";
-	// Scalar multiplication test
-	std::cout << 2.5 * q2 - 3 * q3 << "\n";
-	// Conjugate test
-	std::cout << q3.getConjugate().getConjugate() << "\n";
-	// Hamilton product test
-	std::cout << (q2 * q3).getConjugate() << "\n";
-	std::cout << q3.getConjugate() * q2.getConjugate() << "\n";
-	// Norm test
-	std::cout << (3 * q3).getNorm() << "\n";
-	std::cout << 3 * q3.getNorm() << "\n";
-	// Distance test
-	std::cout << getDistance(q2, q3) << "\n";
-	std::cout << getDistance(q3, q2) << "\n";
+	Quaternion q1(0, 1.f, 0, 0);
+	Quaternion q2(0, 0, 1.f, 0);
+	std::cout << distanceGeodesic(q1, q2) << "\n";
 	return 0;
 }
