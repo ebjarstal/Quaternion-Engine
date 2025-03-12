16a1eb95f92ecc8279c22355d746206aa3615a8d
ericb <ericbmousy@gmail.com>
2025-03-12 18:05:05 +0100

    fix not normalized axis in point rotation

--- quaternions/rotations.cpp
+++ quaternions/rotations.cpp
@@ -1,10 +1,11 @@
 #include "rotations.h"
 
 void rotate_point(Vector3D& point, const Vector3D& axis, const float& angle) {
+	Vector3D normalized_axis{ axis.getNormalized() };
 	Quaternion p_prime{ point };
 	Quaternion q{
 		std::cos(angle / 2.f),
-		axis* std::sin(angle / 2.f)
+		normalized_axis * std::sin(angle / 2.f)
 	};
 	point = (q * p_prime * q.getConjugate()).getVector();
 }
