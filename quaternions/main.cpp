b744b456d06f0326ce612988292fddeeb5ffaa60
ericb <ericbmousy@gmail.com>
2025-03-12 18:05:28 +0100

    add example program for single point rotation

--- quaternions/main.cpp
+++ quaternions/main.cpp
@@ -4,13 +4,38 @@
 #include <iostream>
 #include <math.h>
 
-int main() {
-	Vector3D p   { 0, 2, 2 };
-	Vector3D axis{ 0, 1, 0 };  // y axis
-	const float angle = 3.14159265358979323846f / 2.f;  // pi / 2
+constexpr double g_PI = 3.14159265358979323846;
 
+int main() {
+	Vector3D p;        // point to rotate
+	Vector3D axis;     // rotation axis
+	float angle{ 0 };  // angle of rotation along rotation axis
+
+	// get point coordinates
+	std::cout << "coordonnees du point:\n";
+	std::cout << "x: ";
+	std::cin >> p.x;
+	std::cout << "y: ";
+	std::cin >> p.y;
+	std::cout << "z: ";
+	std::cin >> p.z;
+
+	// get rotation axis
+	std::cout << "vecteur/axe de rotation:\n";
+	std::cout << "x: ";
+	std::cin >> axis.x;
+	std::cout << "y: ";
+	std::cin >> axis.y;
+	std::cout << "z: ";
+	std::cin >> axis.z;
+
+	// get angle
+	std::cout << "angle de rotation (radians): ";
+	std::cin >> angle;
+
+	std::cout << "le point " << p << " a pour coordonnees\n";
 	rotate_point(p, axis, angle);
-	std::cout << p << "\n";
+	std::cout << p << "\napres une rotation de " << angle << " radians autour du vecteur " << axis << "\n";
 
 	return 0;
 }
