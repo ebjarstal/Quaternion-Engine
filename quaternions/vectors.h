115fe2c6b6f2c06507d6ef0852805db439a3dbfd
ericb <ericbmousy@gmail.com>
2025-03-12 16:13:41 +0100

    add short comments to vector functions

--- quaternions/vectors.h
+++ quaternions/vectors.h
@@ -6,6 +6,7 @@ struct Vector3D {
 	float x, y, z;
 
 public:
+	// Returns the Euclidean norm of the vector.
 	const float getNorm() const;
 };
 
@@ -17,5 +18,8 @@ Vector3D operator*(const Vector3D& v, const float& lambda);
 Vector3D operator*(const float& lambda, const Vector3D& v);
 Vector3D operator/(const Vector3D& v, const float& lambda);
 
-float    dotProduct(const Vector3D& v1, const Vector3D& v2);
+// Returns the dot product of two vectors, also known as the scalar product.
+float dotProduct(const Vector3D& v1, const Vector3D& v2);
+
+// Returns the cross product of two vectors.
 Vector3D crossProduct(const Vector3D& v1, const Vector3D& v2);
