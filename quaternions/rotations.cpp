9b5021c721bc38bc1c2bc00b1374d49fde2ca466
ericb <ericbmousy@gmail.com>
2025-03-13 21:58:24 +0100

    rename point rotation function

--- quaternions/rotations.cpp
+++ quaternions/rotations.cpp
@@ -1,6 +1,6 @@
 #include "rotations.h"
 
-void rotate_point(Vector3D& point, const Vector3D& axis, const float& angle) {
+void rotatePoint(Vector3D& point, const Vector3D& axis, const float& angle) {
 	Quaternion q{
 		std::cos(angle / 2.f),
 		axis.getNormalized() * std::sin(angle / 2.f)
--- quaternions/rotations.h
+++ quaternions/rotations.h
@@ -4,4 +4,4 @@
 #include "quaternions.h"
 
 // Rotates the given point along the given axis by the given angle.
-void rotate_point(Vector3D& point, const Vector3D& axis, const float& angle);
+void rotatePoint(Vector3D& point, const Vector3D& axis, const float& angle);
