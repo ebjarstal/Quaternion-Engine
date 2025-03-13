7722d251d2e7d829027b319adc244b410312c9c5
ericb <ericbmousy@gmail.com>
2025-03-13 12:24:31 +0100

    rename projection files

--- quaternions/projection3Dto2D.cpp
+++
@@ -1,8 +0,0 @@
-#include "projection3Dto2D.h"
-
-Vector2D get_camera_to_screen_pos(const Vector3D& p_camera) {
-	return {
-		(float)p_camera.x + g_width/2,
-		(float)p_camera.y + g_height/2
-	};
-}
---
+++ quaternions/projections.cpp
@@ -0,0 +1,9 @@
+#include "projections.h"
+
+Vector2D get_camera_to_screen_pos(const Vector3D& p_camera) {
+	// TODO
+	return {
+		0,
+		0
+	};
+}
