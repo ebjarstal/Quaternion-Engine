07e15ef9a94e40152103b3d4235c0a4b77fd5ebf
ericb <ericbmousy@gmail.com>
2025-03-18 08:31:32 +0100

    add renderline back

--- quaternions/rendering.cpp
+++ quaternions/rendering.cpp
@@ -4,13 +4,17 @@ void renderObject(const Camera3D& camera, const Object& object) {
 	const std::vector<Vector3D> points = object.getPointsGlobal();
 	SDL_SetRenderDrawColor(renderer, object.r, object.g, object.b, object.a);
 	
-	for (int i = 0; i < points.size(); i++) {
+	for (int i = 0; i < points.size() - 1; i++) {
 		const Ray ray1{ points[i], camera.getPositionGlobal() };
 		const Vector2D point1 = camera.getProjectionSurface().getIntersectionPoint(ray1);
-		SDL_RenderPoint(
+		const Ray ray2{ points[i+1], camera.getPositionGlobal() };
+		const Vector2D point2 = camera.getProjectionSurface().getIntersectionPoint(ray2);
+		SDL_RenderLine(
 			renderer,
 			point1.x + g_Width / 2.f,
-			point1.y + g_Height / 2.f
+			point1.y + g_Height / 2.f,
+			point2.x + g_Width / 2.f,
+			point2.y + g_Height / 2.f
 		);
 	}
 }
