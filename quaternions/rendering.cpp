0ec2d71f8aca790a2c727b5db2ef2926b28fabb7
fojd <ericbmousy@gmail.com>
2025-04-13 01:23:55 +0200

    add calculatePixel function for better readability

--- quaternions/rendering.cpp
+++ quaternions/rendering.cpp
@@ -1,20 +1,22 @@
 #include "rendering.h"
 
+Vector2D calculatePixel(const Camera3D& camera, const Vector3D& point) {
+	return camera.getProjectionSurface().getIntersectionPoint({ point, camera.getPositionGlobal() });
+}
+
 void renderObject(const Camera3D& camera, const Object& object) {
 	const std::vector<Vector3D> points = object.getPointsGlobal();
 	SDL_SetRenderDrawColor(renderer, object.r, object.g, object.b, object.a);
 	
 	for (int i = 0; i < points.size() - 1; i++) {
-		const Ray ray1{ points[i], camera.getPositionGlobal() };
-		const Vector2D point1 = camera.getProjectionSurface().getIntersectionPoint(ray1);
-		const Ray ray2{ points[i+1], camera.getPositionGlobal() };
-		const Vector2D point2 = camera.getProjectionSurface().getIntersectionPoint(ray2);
+		const Vector2D pixel1 = calculatePixel(camera, points[i]);
+		const Vector2D pixel2 = calculatePixel(camera, points[i + 1]);
 		SDL_RenderLine(
 			renderer,
-			point1.x + g_Width / 2.f,
-			point1.y + g_Height / 2.f,
-			point2.x + g_Width / 2.f,
-			point2.y + g_Height / 2.f
+			pixel1.x + g_Width / 2.f,
+			pixel1.y + g_Height / 2.f,
+			pixel2.x + g_Width / 2.f,
+			pixel2.y + g_Height / 2.f
 		);
 	}
 }
--- quaternions/rendering.h
+++ quaternions/rendering.h
@@ -14,5 +14,8 @@ extern const int g_Height;
 extern       SDL_Renderer*    renderer;
 extern const CoordinateSystem g_WorldCoordinateSystem;
 
-// Renders the given object using the global renderer.
+// Calculates the coordinates of a pixel that should be drawn onto the projection surface of the given camera
+Vector2D calculatePixel(const Camera3D& camera, const Vector3D& point);
+
+// Renders the given object onto the camera's projection surface using the global renderer.
 void renderObject(const Camera3D& camera, const Object& object);
